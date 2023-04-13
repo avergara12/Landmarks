edb32c4f4898aa486c6daab9dc34c59c7e98f30d
Alexandra <Avergara12@cps.edu>
2023-04-13 14:02:39 -0500

    4/13/23

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -1,35 +1,28 @@
-//
-//  Landmark.swift
-//  Landmarks
-//
-//  Created by Vergara, Alexandra on 3/28/23.
-//
-
 import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
-    var State: String
+    var state: String
     var description: String
-    
+
     private var imageName: String
     var image: Image {
         Image(imageName)
     }
-    
+
     private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
-            longitude: coordinates.longtitude)
+            longitude: coordinates.longitude)
     }
-    
+
     struct Coordinates: Hashable, Codable {
         var latitude: Double
-        var longtitude: Double
+        var longitude: Double
     }
 }
--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -1,22 +1,26 @@
-//
-//  LandmarkRow.swift
-//  Landmarks
-//
-//  Created by Vergara, Alexandra on 3/30/23.
-//
-
 import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: Landmarks[0])
+        Group {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+        .previewLayout(.fixed(width: 300, height: 70))
     }
 }
