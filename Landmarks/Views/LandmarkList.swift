59e9e9686ebb306df91633a577e5d5e24ab2ff84
Alexandra <Avergara12@cps.edu>
2023-04-17 14:01:29 -0500

    4/17/23

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -1,11 +1,18 @@
 import SwiftUI
 
 struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            ( showFavoritesOnly || landmark.isFavorite)
+        }
+    }
     var body: some View {
         NavigationView {
             List(landmarks) { landmark in
                 NavigationLink {
-                    LandmarkDetail()
+                    LandmarkDetail(landmark: landmark)
                 } label: {
                     LandmarkRow(landmark: landmark)
                 }
