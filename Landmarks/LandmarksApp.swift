d84c8bed199242ce4eef1a4af733dd5282aba051
Alexandra <Avergara12@cps.edu>
2023-04-19 14:02:34 -0500

    4/19/23

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -1,17 +1,13 @@
-//
-//  LandmarksApp.swift
-//  Landmarks
-//
-//  Created by Vergara, Alexandra on 3/23/23.
-//
-
 import SwiftUI
 
 @main
 struct LandmarksApp: App {
+    @StateObject private var modelData = ModelData()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(modelData)
         }
     }
 }
